5b3291358a95af4e8cbeff0f5ab24fa054e4d173
Your NameErik <erik.deschenes@gmail.com>
2025-12-28 13:56:34 -0500

    fix(ui): supprimer double affichage de la main d'oeuvre

--- flip/admin/projets/partials/tab-base.php
+++ flip/admin/projets/partials/tab-base.php
@@ -608,24 +608,6 @@ if ($isPartialBase) {
     </td>
 </tr>
 
-<!-- MAIN D'ŒUVRE -->
-<?php
-$diffMO = $moExtrapole['cout'] - $moReel['cout'];
-if ($moExtrapole['heures'] > 0 || $moReel['heures'] > 0):
-?>
-<tr class="sub-item labor-row">
-    <td>
-        <i class="bi bi-person-fill me-1"></i>Main d'œuvre
-        <small class="d-block opacity-75">
-            Planifié: <?= number_format($moExtrapole['heures'], 0) ?>h (<?= $moExtrapole['jours'] ?>j) |
-            Réel: <?= number_format($moReel['heures'], 1) ?>h
-        </small>
-    </td>
-    <td class="text-end"><?= formatMoney($moExtrapole['cout']) ?></td>
-    <td class="text-end <?= $diffMO >= 0 ? 'positive' : 'negative' ?>"><?= formatMoney($diffMO) ?></td>
-    <td class="text-end"><?= formatMoney($moReel['cout']) ?></td>
-</tr>
-<?php endif; ?>
 <!-- RÉNOVATION_DYNAMIC_START -->
 <?php
                     $totalBudgetReno = 0;
